ef6b828990b1af86403b274596f08db47096173f
Martin Scharm <martin@binfalse.de>
2015-06-18 13:35:44 +0100

    updated bundling script as we will have more files

--- createbundle.sh
+++ createbundle.sh
@@ -14,10 +14,14 @@ echo -n application/vnd.wf4ever.robundle+zip > mimetype
 zip -q -0 "$BUNDLENAME" mimetype
 
 # add all other files with the default compression
-zip -q -r "$BUNDLENAME" . -x mimetype "$BUNDLENAME" .git\*
+zip -q -r "$BUNDLENAME" . -x mimetype "$BUNDLENAME" .git\* \*.tex.backup \*.bib.backup \*.out \*.aux \*.log
 
 # finally list the contents of the bundle
-unzip -l "$BUNDLENAME"
+echo " ------------------------------------------- "
+echo "research bundled in $BUNDLENAME"
+echo "bundle contains:"
+echo " ------------------------------------------- "
+unzip -l "$BUNDLENAME" | /bin/grep "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]" | sort -k4 -d
 
 
 
